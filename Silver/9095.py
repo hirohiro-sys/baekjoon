2d8abda62c94e734199bc44757bfc3feb0696503
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2023-12-28 09:46:29 +0900

    <동적계획법>

---
+++ Silver/9095.py
@@ -0,0 +1,10 @@
+T = int(input())
+A = [int(input()) for _ in range(T)]
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4,11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+for i in A:
+    print(dp[i])
