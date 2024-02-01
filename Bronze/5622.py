e5845b934120b437070b6d3f6a77d508a7868da1
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-02-01 09:10:25 +0900

    <구현>

---
+++ Bronze/5622.py
@@ -0,0 +1,16 @@
+s = input()
+ans = 0
+for i in s:
+    if i in "ABC":    ans += 3
+    elif i in "DEF":  ans += 4
+    elif i in "GHI":  ans += 5
+    elif i in "JKL":  ans += 6
+    elif i in "MNO":  ans += 7
+    elif i in "PQRS": ans += 8
+    elif i in "TUV":  ans += 9
+    elif i in "WXYZ": ans += 10
+print(ans)
+
+"""
+https://www.acmicpc.net/problem/5622
+"""
