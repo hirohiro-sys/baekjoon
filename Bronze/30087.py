99a24104607770a99ad8326ebd4778c76f5933f0
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-04-10 22:27:02 +0900

    <조건분기>

---
+++ Bronze/30087.py
@@ -0,0 +1,21 @@
+#Algorithm	204
+#DataAnalysis	207
+#ArtificialIntelligence	302
+#CyberSecurity	B101
+#Network	303
+#Startup	501
+#TestStrategy	105
+
+for i in range(int(input())):
+    s = input()
+    if s=="Algorithm": print(204)
+    elif s=="DataAnalysis": print(207)
+    elif s=="ArtificialIntelligence": print(302)
+    elif s=="CyberSecurity": print("B101")
+    elif s=="Network": print(303)
+    elif s=="Startup": print(501)
+    elif s=="TestStrategy": print(105)
+
+"""
+https://www.acmicpc.net/problem/30087
+"""
