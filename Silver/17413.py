75dd450aabfd66cbd3fc0728f4e1940c59ac5b33
hirohiro-sys <126783940+hirohiro-sys@users.noreply.github.com>
2024-07-18 09:16:14 +0900

    <stack & 구현>

--- Silver/17413.py
+++ Silver/17413.py
@@ -1,22 +1,15 @@
 s = list(input())
-
 i = 0
-start = 0
-while i < len(s):
-    # <>内の処理
+while i<len(s):
     if s[i]=="<":
-        while i<len(s) and s[i]!=">":
-            i += 1
-        i += 1
-    # <>外の処理
+        i = s.index(">",i)+1
     elif s[i].isalnum():
         start = i
         while i<len(s) and s[i].isalnum():
             i += 1
-        s[start:i] = s[start:i][::-1]
-    else: 
+        s[start:i] = reversed(s[start:i])
+    else:
         i += 1
-
 print("".join(s))
 
 # https://www.acmicpc.net/problem/17413
